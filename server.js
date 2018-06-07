b6836189d42486a6378a2b33ad53e3e3ad8dea4f
Xuan Huy Vo <Xuan.HuyVo@MY.ALLIANZ.DIR>
2018-06-07 19:51:11 +0800

    include eslint

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const open = require('open');
+const compression = require('compression');
 const port = process.env.PORT || 1607;
 const app = express();
 
 app.use(bodyParser.json()); //for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 app.use( express.static( `${__dirname}/dist` ) );
+app.use(compression());
 app.get('/', function (request, response) {
 	// response.send('Hello World..!! From Express APP..!!');
 	response.sendFile(__dirname + '/dist/index.html');
@@ -14,12 +16,7 @@ app.get('/', function (request, response) {
 app.get('*', function (request, response) {
 	response.sendFile(__dirname + '/dist/index.html');
 });
-app.get('*.js', function (req, res, next) {
-	debugger;
-	req.url = req.url + '.gz';
-	res.set('Content-Encoding', 'gzip');
-	next();
-});
+
 // app.get('/about', function (request, response) {
 // 	response.sendFile(__dirname + '/dist/index.html');
 // });
