17ee3976dcb32b5c186bf79da45e9b65e6f29a8c
Harry Vo <huyvoxuan8489@gmail.com>
2018-06-11 23:57:14 +0800

    conflit code

--- server.js
+++ server.js
@@ -7,22 +7,17 @@ const app = express();
 
 app.use(bodyParser.json()); //for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
-app.use( express.static( `${__dirname}/dist` ) );
 
-app.use(compression());
 app.get('/', function (request, response) {
-	// response.send('Hello World..!! From Express APP..!!');
 	response.sendFile(__dirname + '/dist/index.html');
 });
+
+app.use( express.static( `${__dirname}/dist` ) );
+app.use(compression());
+
 app.get('*', function (request, response) {
 	response.sendFile(__dirname + '/dist/index.html');
 });
-// app.get('/about', function (request, response) {
-// 	response.sendFile(__dirname + '/dist/index.html');
-// });
-// app.get('/contact', function (request, response) {
-// 	response.sendFile(__dirname + '/dist/index.html');
-// });
 
 app.listen(port, function () {
 	const message = `App listening on port ${port}`;
